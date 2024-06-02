30453e4747d729bffeeaf61cf7274252b6f2e1cc
Julio Adrian Ferreyra <jaft210403@outlook.com>
2024-06-01 20:33:51 -0600

    validaciones terminadas

--- Productos/editar.js
+++ Productos/editar.js
@@ -17,36 +17,43 @@ function cargar_datos() {
     url: "datos_productos.php",
     method: "POST",
     data: {
-        productoId: $("#selectProductos").val(),
+      productoId: $("#selectProductos").val(),
     },
     success: function (res) {
-        var data = JSON.parse(res);
-        
-        $("#txtProducto").val(data.nombre);
-        $("#selCategoria").val(data.categoria);
-        $("#txtCompra").val(data.compra);
-        $("#txtVenta").val(data.venta);
+      var data = JSON.parse(res);
+
+      $("#txtProducto").val(data.nombre);
+      $("#selCategoria").val(data.categoria);
+      $("#txtCompra").val(data.compra);
+      $("#txtVenta").val(data.venta);
     },
   });
 }
 
-function actualizar_producto(){
-    $.ajax({
-        url: "editar_producto.php",
-        method: "POST",
-        data: {
-          id: $("#selectProductos").val(),
-          nombre: $("#txtProducto").val(),
-          categoria: $("#selCategoria").val(),
-          compra: $("#txtCompra").val(),
-          venta: $("#txtVenta").val(),
-          
+function actualizar_producto() {
+    let compra = Number($("#txtCompra").val());
+    let venta = Number($("#txtVenta").val());
 
-        },
-        success: function(res){
-          alert(res);
-        }
-      });
+  if (venta <=  compra) {
+    alert(
+      "No es posible realizar el registro:\n'El precio de compra debe ser menor al precio de venta' "
+    );
+  } else {
+    $.ajax({
+      url: "editar_producto.php",
+      method: "POST",
+      data: {
+        id: $("#selectProductos").val(),
+        nombre: $("#txtProducto").val(),
+        categoria: $("#selCategoria").val(),
+        compra: $("#txtCompra").val(),
+        venta: $("#txtVenta").val(),
+      },
+      success: function (res) {
+        alert(res);
+      },
+    });
+  }
 }
 
 $(document).ready(function () {
