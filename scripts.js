b7747262809c8130b6f743ef3dea3504324855d8
Julio Adrian Ferreyra <jaft210403@outlook.com>
2024-06-04 02:12:26 -0600

    bases_home

--- scripts.js
+++ scripts.js
@@ -1,66 +1,70 @@
-function regresar(){
-    window.location.href = "index.html";
+function regresar() {
+  window.location.href = "index.html";
 }
 
 function iniciar_sesion() {
   let usuario = $("#txtUsuario").val();
   let pass = $("#txtPass").val();
 
-  $.ajax({
-    url: "login.php",
-    method: "POST",
-    data: {
-      usuario: usuario,
-      password: pass,
-    },
-    success: function (res) {
-      if (res == 0) {
-        alert("Usuario invalido.\nIntente de nuevo, por favor");
-      } else {
-        window.location.href = "inicio.html";
-      }
-    },
-  });
+  if (usuario === "" || pass === "") {
+    alert("Campos vacios");
+  } else {
+    $.ajax({
+      url: "login.php",
+      method: "POST",
+      data: {
+        usuario: usuario,
+        password: pass,
+      },
+      success: function (res) {
+        if (res == 0) {
+          alert("Usuario invalido.\nIntente de nuevo, por favor");
+        } else {
+          window.location.href = "inicio.html";
+        }
+      },
+    });
+  }
 }
 
-function registar_usuario(){
-let lblError = document.getElementById("lblError");
-let nombre = $("#nombre").val();
-let apellido = $("#apellido").val();
-let fec_nac = $("#fec_nac").val();
-let telefono = $("#telefono").val();
-let usuario =$("#usuario").val();
-let pass = $("#pass").val();
-let c_pass = $("#c_pass").val();
+function registar_usuario() {
+  let lblError = document.getElementById("lblError");
+  let nombre = $("#nombre").val();
+  let apellido = $("#apellido").val();
+  let fec_nac = $("#fec_nac").val();
+  let telefono = $("#telefono").val();
+  let usuario = $("#usuario").val();
+  let pass = $("#pass").val();
+  let c_pass = $("#c_pass").val();
 
-if(nombre === "" || apellido === "" || fec_nac === "" || telefono === "", usuario === "", pass === "" , c_pass === ""){
+  if (
+    (nombre === "" || apellido === "" || fec_nac === "" || telefono === "",
+    usuario === "",
+    pass === "",
+    c_pass === "")
+  ) {
     lblError.textContent = "Campos vacios";
     lblError.style.color = "red";
-}
-else if(pass !== c_pass){
+  } else if (pass !== c_pass) {
     lblError.textContent = "Contraseñas no coinciden";
     lblError.style.color = "red";
-}
-else{
+  } else {
     $.ajax({
-        url: "nuevo_usuario.php",
-        method: "POST",
-        data: {
-            nombre: nombre,
-            apellido: apellido,
-            fec_nac: fec_nac,
-            telefono: telefono,
-            usuario: usuario,
-            pass: pass,
-        },
-        success: function (res) {
-            lblError.textContent = "";
-            alert(res);
-            window.location.href = "index.html";
-   
-        }
-    
+      url: "nuevo_usuario.php",
+      method: "POST",
+      data: {
+        nombre: nombre,
+        apellido: apellido,
+        fec_nac: fec_nac,
+        telefono: telefono,
+        usuario: usuario,
+        pass: pass,
+      },
+      success: function (res) {
+        lblError.textContent = "";
+        alert(res);
+        window.location.href = "index.html";
+      },
     });
-}
-
+  }
 }
